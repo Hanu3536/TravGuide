2d1b56a3c4b2a13507cde441e945f2a3f0506ff4
Hanumantharao Chadalavada <hanuca3536@gmail.com>
2022-11-18 01:24:53 -0800

    # added speec to text feature

--- app/src/main/java/com/capstone/travguide/TravisHomePageFragment.kt
+++ app/src/main/java/com/capstone/travguide/TravisHomePageFragment.kt
@@ -1,26 +1,32 @@
 package com.capstone.travguide
 
 import android.Manifest
+import android.R.attr
+import android.app.Activity.RESULT_OK
 import android.content.Context
+import android.content.Intent
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.capstone.travguide.databinding.FragmentTravisHomePageBinding
+import java.util.*
 
 
 class TravisHomePageFragment : Fragment() {
 
     private lateinit var binding: FragmentTravisHomePageBinding
+    private lateinit var onSpeechTextResult: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,13 +37,54 @@ class TravisHomePageFragment : Fragment() {
         return binding.root
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        onSpeechTextResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val result: ArrayList<String> =
+                    it.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                binding.tvSpeechText.text = Objects.requireNonNull(result)[0]
+                binding.tvSpeechText.visibility = View.VISIBLE
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.ivProfile.setOnClickListener {
             findNavController().navigateUp()
         }
+
+        binding.ivTextMic.setOnClickListener {
+            speechToTextConvert()
+        }
 //        locationPermissionRequesting()
+    }
+
+    private fun speechToTextConvert() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE,
+            Locale.getDefault()
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now")
+
+        try {
+            onSpeechTextResult.launch(intent)
+        } catch (e: Exception) {
+            Toast
+                .makeText(
+                    this@TravisHomePageFragment.context, " " + e.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+        }
     }
 
     private fun locationPermissionRequesting() {
